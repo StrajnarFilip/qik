6cfd6c18223e8131711a5c10fd61c12b8bbac095
Filip Strajnar <filip.strajnar@gmail.com>
2022-12-07 00:26:34 +0100

    clean

--- mix.exs
+++ mix.exs
@@ -4,10 +4,14 @@ defmodule Qik.MixProject do
   def project do
     [
       app: :qik,
-      version: "0.1.0",
+      version: "0.0.1",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      package: package(),
+      name: "Qik",
+      source_url: "https://github.com/StrajnarFilip/qik",
+      description: "Quick utilities, when they are needed the most."
     ]
   end
 
@@ -18,9 +22,17 @@ defmodule Qik.MixProject do
     ]
   end
 
+  def package() do
+    [
+      licenses: ["Apache-2.0"],
+      links: %{"GitHub" => "https://github.com/StrajnarFilip/qik"}
+    ]
+  end
+
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:ex_doc, "~> 0.27", only: :dev, runtime: false},
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
